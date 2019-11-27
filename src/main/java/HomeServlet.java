ec931a742e4033aa566ccc8e11442d4b007cd33c
Aleksey <aljasones@yandex.ru>
2019-11-27 21:22:23 +0300

    add session (count)

--- src/main/java/HomeServlet.java
+++ src/main/java/HomeServlet.java
@@ -2,6 +2,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -9,6 +10,17 @@ public class HomeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+
+        Integer count = (Integer) session.getAttribute("count");
+
+        if (count == null) {
+            session.setAttribute("count", 1);
+            count = 1;
+        } else {
+            session.setAttribute("count", count + 1);
+        }
+
         String name = req.getParameter("name");
         String surname = req.getParameter("surname");
 
@@ -16,6 +28,7 @@ public class HomeServlet extends HttpServlet {
 
         pw.println("<html>");
         pw.println("<h1> Hello, " + name + " " + surname + " </h1>");
+        pw.println("<h3> Your count in this page is: " + count + " </h3>");
         pw.println("</html>");
 
     }
---
+++ src/main/webapp/WEB-INF/error.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Алексей Владимирович
+  Date: 25.11.2019
+  Time: 16:10
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>ERROR</title>
+</head>
+<body>
+    <h1>
+        Some error happened
+    </h1>
+
+</body>
+</html>
